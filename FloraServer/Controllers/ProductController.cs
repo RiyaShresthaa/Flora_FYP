488fcad0db33c15de2b5b418480db94c69b02847
RiyaShresthaa <riyashrestha279@gmail.com>
2024-02-19 20:47:52 +0545

    productcontroller changes

--- FloraServer/Controllers/ProductController.cs
+++ FloraServer/Controllers/ProductController.cs
@@ -10,24 +10,24 @@ namespace FloraServer.Controllers
     [ApiController]
     public class ProductController : ControllerBase
     {
-        private readonly IProduct _productService;
+        private readonly IProduct productService;
 
         public ProductController(IProduct productService)
         {
-            _productService = productService;
+            this.productService = productService;
         }
 
         [HttpGet]
         public async Task<ActionResult<List<Product>>> GetAllProducts(bool featured)
         {
-            var products = await _productService.GetAllProducts(featured); return Ok(products);
+            var products = await productService.GetAllProducts(featured); return Ok(products);
         }
 
         [HttpPost]
         public async Task<ActionResult<ServiceResponse>> AddProduct(Product model)
         {
             if (model is null) return BadRequest("Model is Null");
-            var response = await _productService.AddProduct(model);
+            var response = await productService.AddProduct(model);
             return Ok(response);
         }
     }
