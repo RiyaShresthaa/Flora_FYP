826dc12ca9713448c32bd0514524f0bbe64e6c19
RiyaShresthaa <riyashrestha279@gmail.com>
2024-02-24 12:58:32 +0545

    created client services

---
+++ FloraClient/Services/ClientServices.cs
@@ -0,0 +1,20 @@
+﻿using FloraSharedLibrary.Contracts;
+using FloraSharedLibrary.Models;
+using FloraSharedLibrary.Responses;
+
+namespace FloraClient.Services
+{
+    public class ClientServices(HttpClient httpClient) : IProduct
+    {
+        private const string BaseUrl = "api/product";
+        public Task<ServiceResponse> AddProduct(Product model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Product>> GetAllProducts(bool featuredProducts)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
