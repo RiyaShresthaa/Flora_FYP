18437475e0ee2df79c7669d5a058e7ea7c7d0bc5
RiyaShresthaa <riyashrestha279@gmail.com>
2024-02-24 12:20:12 +0545

    fixed add product service error

--- FloraSharedLibrary/Models/Product.cs
+++ FloraSharedLibrary/Models/Product.cs
@@ -15,7 +15,7 @@ namespace FloraSharedLibrary.Models
         public decimal Price { get; set; }
         [Required, DisplayName("Product Image")]
         public string? Base64Img { get; set; }
-        [Required, Range(0, 99999)]
+        [Required, Range(1, 99999)]
         public int Quantity { get; set; }
         public bool Featured { get; set; } = false;
         public DateTime DateUploaded { get; set; } = DateTime.Now;
