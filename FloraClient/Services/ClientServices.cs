fdda37c06bde60ac05d1a8877ba918f57d9de861
RiyaShresthaa <riyashrestha279@gmail.com>
2024-02-24 20:51:34 +0545

    created product service

--- FloraClient/Services/ClientServices.cs
+++ FloraClient/Services/ClientServices.cs
@@ -1,20 +1,48 @@
 ﻿using FloraSharedLibrary.Contracts;
 using FloraSharedLibrary.Models;
 using FloraSharedLibrary.Responses;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace FloraClient.Services
 {
     public class ClientServices(HttpClient httpClient) : IProduct
     {
         private const string BaseUrl = "api/product";
-        public Task<ServiceResponse> AddProduct(Product model)
+        private static string SerializedObj(object modelObject) => JsonSerializer.Serialize(modelObject, JsonOptions());
+        private static T DeserializeJsonString<T>(string jsonString) => JsonSerializer.Deserialize<T>(jsonString, JsonOptions())!;
+        private static StringContent GenerateStringContent(string serialiazedObj) => new(serialiazedObj, System.Text.Encoding.UTF8, "application/json");
+        private static IList<T> DeserializeJsonStringList<T>(string jsonString) => JsonSerializer.Deserialize<IEnumerable<T>>(jsonString, JsonOptions())!;
+        private static JsonSerializerOptions JsonOptions()
         {
-            throw new NotImplementedException();
+            return new JsonSerializerOptions
+            {
+                AllowTrailingCommas = true,
+                PropertyNameCaseInsensitive = true,
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                UnmappedMemberHandling = JsonUnmappedMemberHandling.Skip
+            };
         }
 
-        public Task<List<Product>> GetAllProducts(bool featuredProducts)
+
+        public async Task<ServiceResponse> AddProduct(Product model)
         {
-            throw new NotImplementedException();
+            var response = await httpClient.PostAsync(BaseUrl, GenerateStringContent(SerializedObj(model)));
+
+            if (!response.IsSuccessStatusCode)
+                return new ServiceResponse(false, "Error occured. Try again later...");
+
+            var apiResponse = await response.Content.ReadAsStringAsync();
+            return DeserializeJsonString<ServiceResponse>(apiResponse);
+        }
+
+        public async Task<List<Product>> GetAllProducts(bool featuredProducts)
+        {
+            var response = await httpClient.GetAsync($"{BaseUrl}?featured={featuredProducts}");
+            if (!response.IsSuccessStatusCode) return null!;
+
+            var result = await response.Content.ReadAsStringAsync();
+            return [.. DeserializeJsonStringList<Product>(result)];
         }
     }
 }
