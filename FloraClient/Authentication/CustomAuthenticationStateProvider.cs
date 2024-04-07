cb6db8038de6adfb931f0c2b27fd7d9fbace22cd
RiyaShresthaa <riyashrestha279@gmail.com>
2024-04-07 16:56:28 +0545

    Authentication runtime error

---
+++ FloraClient/Authentication/CustomAuthenticationStateProvider.cs
@@ -0,0 +1,48 @@
+﻿using FloraClient.Services;
+using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+
+namespace FloraClient.Authentication
+{
+    public class CustomAuthenticationStateProvider(AuthenticationService authenticationService) : AuthenticationStateProvider
+    {
+        private ClaimsPrincipal anonymous = new(new ClaimsIdentity());
+
+        public async override Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            try
+            {
+                var getUserSession = await authenticationService.GetUserDetails();
+
+                if (getUserSession is null || string.IsNullOrEmpty(getUserSession.Email)) 
+                    return await Task.FromResult(new AuthenticationState(anonymous));
+
+                var claimsPrincipal = authenticationService.SetClaimPrincipal(getUserSession);
+                return await Task.FromResult(new AuthenticationState(claimsPrincipal));
+            }
+            catch
+            {
+                return await Task.FromResult(new AuthenticationState(anonymous));
+
+            }
+        }
+
+        public async Task UpdateAuthentication(TokenProp tokenProp)
+        {
+            ClaimsPrincipal claimsPrincipal = new();
+            if (tokenProp is not null || !string.IsNullOrEmpty(tokenProp!.Token))
+            {
+                await authenticationService.SetTokenToLocalStorage(General.SerializedObj(tokenProp));
+                var getUserSession = await authenticationService.GetUserDetails();
+                if (getUserSession is not null || !string.IsNullOrEmpty(getUserSession!.Email))
+                    claimsPrincipal = authenticationService.SetClaimPrincipal(getUserSession);
+            }
+            else
+            {
+                claimsPrincipal = anonymous;
+                await authenticationService.RemoveTokenFromLocalStorage();
+            }
+            NotifyAuthenticationStateChanged(Task.FromResult(new AuthenticationState(claimsPrincipal)));
+        }
+    }
+}
